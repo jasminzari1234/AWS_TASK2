2140e5f9eac6e63d0dfcb7641f2c38f8e59b621c
jasminzari1234 <62955506+jasminzari1234@users.noreply.github.com>
2020-07-21 00:18:35 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -20,3 +20,34 @@ output "b" {
   value = aws_s3_bucket.b
 }
 
+// Creating Origin Access Identity
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+  comment = "Some comment"
+}
+output "origin_access_identity" {
+  value = aws_cloudfront_origin_access_identity.origin_access_identity
+}
+
+// Creating bucket policy
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.b.arn}/*"]
+    principals {
+      type        = "AWS"
+      identifiers = ["${aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn}"]
+    }
+  }
+  statement {
+    actions   = ["s3:ListBucket"]
+    resources = ["${aws_s3_bucket.b.arn}"]
+    principals {
+      type        = "AWS"
+      identifiers = ["${aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn}"]
+    }
+  }
+}
+resource "aws_s3_bucket_policy" "example" {
+  bucket = aws_s3_bucket.b.id
+  policy = data.aws_iam_policy_document.s3_policy.json
+}
