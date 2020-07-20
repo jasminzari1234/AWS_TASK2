a5150f93ce0b3bd20459804f87198eae799cedf8
jasminzari1234 <62955506+jasminzari1234@users.noreply.github.com>
2020-07-21 00:18:09 +0530

    Create cloudfront.tf

---
+++ cloudfront.tf
@@ -0,0 +1,44 @@
+
+// Creating CloudFront
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.b.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
+    }
+  }
+  enabled             = true
+  is_ipv6_enabled     = true
+ default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+//add image in bucket
+resource "null_resource" "null4"  {
+
+          provisioner "local-exec" {
+	    command = "aws s3 cp C:/Users/ABC/Downloads/successCloudNew.svg s3://jazbucket --acl public-read"
+            
+  	}
+}
